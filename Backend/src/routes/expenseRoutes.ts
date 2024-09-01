a824df0e02426f3b221ce85b7fabca42356055dd
razaakomal <komal.raza@leoatech.com>
2024-09-01 17:23:56 +0500

    updated the backend part

--- Backend/src/routes/expenseRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { getExpenseByCategory } from "../controllers/expenseController";
-
-const router = Router();
-
-router.get("/", getExpenseByCategory);
-
-export default router;
